a16aad162ed864989a72bd5ed4494b5dd79c838a
Davide Venturelli <dventurelli90@gmail.com>
2022-03-24 16:17:15 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 //debug
 console.log('JS OK!');
+
+const hello = new Vue(
+    {
+        el: '#root',
+        data: {
+            testo: 'Ciao, Questo è VUE JS!',
+            immagine: 'meme.jpg',
+        }
+    }
+)
